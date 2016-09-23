264c7128d626f31f3e94fbe320122fb7743b143b
Mikhail Nikolskiy <mikky-nikol@mail.ru>
2016-09-23 10:46:16 +0300

    third commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	int a=0;
+	int b=0;
+	cout<< "Введите два числа";
+	cin>> a;
+	cin>> b;
+
 	return 0;
 }
